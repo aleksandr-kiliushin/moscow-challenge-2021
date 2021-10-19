b9592c25c7e54fcefd0329c68d79f9ba39ab3ae6
a.kilyushin <a.kilyushin@rambler-co.ru>
2021-10-19 12:34:59 +0300

    add fake legend

--- src/views/Legend/index.tsx
+++ src/views/Legend/index.tsx
@@ -1,15 +1,50 @@
 import React from 'react'
 
-// Data
-// import ao from '../data/mo.json'
+// Styles
+import s from './index.module.css'
 
 export const Legend = () => {
+	const legendData: ILegendDataItem[] = [
+		{
+			backgroundColor: 'green',
+			color: 'white',
+			maxOverPopulation: 99,
+			minOverPopulation: 0,
+		},
+		{
+			backgroundColor: 'yellow',
+			color: 'black',
+			maxOverPopulation: 179,
+			minOverPopulation: 100,
+		},
+		{
+			backgroundColor: 'red',
+			color: 'black',
+			maxOverPopulation: 249,
+			minOverPopulation: 180,
+		},
+		{
+			backgroundColor: 'darkred',
+			color: 'white',
+			maxOverPopulation: 300,
+			minOverPopulation: 250,
+		},
+	]
+
 	return (
-		<div>hehe</div>
-		// <ul>
-		// 	{ao.features.map((feature) => (
-		// 		<li key={feature.properties.NAME}>{feature.properties.NAME}</li>
-		// 	))}
-		// </ul>
+		<div className={s.Legend}>
+			{legendData.map(({ backgroundColor, color, maxOverPopulation, minOverPopulation }, index) => (
+				<div className={s.LegendItem} key={index} style={{ backgroundColor, color }}>
+					{minOverPopulation}% - {maxOverPopulation}%
+				</div>
+			))}
+		</div>
 	)
+}
+
+interface ILegendDataItem {
+	backgroundColor: string
+	color: string
+	maxOverPopulation: number
+	minOverPopulation: number
 }
