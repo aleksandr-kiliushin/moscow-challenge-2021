5d88277ba0f22d52cfa8710d40557300e67860e9
Alexander <alexander-kilyushin@gmail.com>
2021-10-24 14:38:33 +0300

    working on map

--- frontend/src/views/index.tsx
+++ frontend/src/views/index.tsx
@@ -23,6 +23,9 @@ import existingSchoolSvg from '../assets/existing-school.svg'
 import { Feature, Geometry } from 'geojson'
 import { ISchool } from '#models/map'
 
+// Data
+import cellsData from '../data/cells.json'
+
 export const App = () => {
 	const dispatch = useAppDispatch()
 
@@ -34,6 +37,14 @@ export const App = () => {
 	)
 	const municipalDistrictsData = useAppSelector((state) => state.map.municipalDistrictsData)
 	const schoolsData = useAppSelector((state) => state.map.existingSchoolsData)
+
+	const getColor = (value: number) => {
+		//value from 0 to 1
+		// var hue = ((1 - value) * 120).toString(10)
+		// return ['hsl(', hue, ',100%,50%)'].join('')
+
+		return 'red'
+	}
 
 	useEffect(() => {
 		// Инициализируем получение данных при запуске приложения.
@@ -51,73 +62,87 @@ export const App = () => {
 	}, [])
 
 	// Наносим на карту административные районы.
-	useEffect(() => {
-		Leaflet.geoJSON(administrativeDistrictsData, {
-			style: {
-				color: 'black',
-				fill: false,
-				weight: 5,
-			},
-		}).addTo(map.current as Map)
-	}, [administrativeDistrictsData])
+	// useEffect(() => {
+	// 	Leaflet.geoJSON(administrativeDistrictsData, {
+	// 		style: {
+	// 			color: 'black',
+	// 			fill: false,
+	// 			weight: 5,
+	// 		},
+	// 	}).addTo(map.current as Map)
+	// }, [administrativeDistrictsData])
 
-	// Наносим на карту муниципальные районы.
-	useEffect(() => {
-		const onEachFeature = (district: Feature<Geometry, any>, layer: Layer) => {
-			layer.bindPopup(district.properties.NAME, {
-				maxWidth: 450,
-			})
-		}
+	// // Наносим на карту муниципальные районы.
+	// useEffect(() => {
+	// 	const onEachFeature = (district: Feature<Geometry, any>, layer: Layer) => {
+	// 		layer.bindPopup(district.properties.NAME, {
+	// 			maxWidth: 450,
+	// 		})
+	// 	}
 
-		Leaflet.geoJSON(municipalDistrictsData, {
-			onEachFeature,
-			style: {
-				color: 'black',
-				fillColor: 'black',
-				fillOpacity: 0.1,
-				weight: 2,
-			},
-		}).addTo(map.current as Map)
-	}, [municipalDistrictsData])
+	// 	Leaflet.geoJSON(municipalDistrictsData, {
+	// 		onEachFeature,
+	// 		style: {
+	// 			color: 'black',
+	// 			fillColor: 'black',
+	// 			fillOpacity: 0.1,
+	// 			weight: 2,
+	// 		},
+	// 	}).addTo(map.current as Map)
+	// }, [municipalDistrictsData])
 
-	// Наносим на карту существующие школы.
-	useEffect(() => {
-		const onEachFeature = (school: Feature<Geometry, ISchool['properties']>, layer: Layer) => {
-			layer.bindPopup(school.properties.name, {
-				maxWidth: 450,
-			})
+	// // Наносим на карту существующие школы.
+	// useEffect(() => {
+	// 	const onEachFeature = (school: Feature<Geometry, ISchool['properties']>, layer: Layer) => {
+	// 		layer.bindPopup(school.properties.name, {
+	// 			maxWidth: 450,
+	// 		})
 
-			// 	// @ts-ignore
-			// 	Leaflet.circle([...school.geometry.coordinates].reverse(), {
-			// 		color: 'white',
-			// 		fillColor: 'white',
-			// 		fillOpacity: 0.7,
-			// 		radius: school.properties.optimalStudentsAmount * 2,
-			// 	}).addTo(map.current as Map)
-		}
+	// 		// 	// @ts-ignore
+	// 		// 	Leaflet.circle([...school.geometry.coordinates].reverse(), {
+	// 		// 		color: 'white',
+	// 		// 		fillColor: 'white',
+	// 		// 		fillOpacity: 0.7,
+	// 		// 		radius: school.properties.optimalStudentsAmount * 2,
+	// 		// 	}).addTo(map.current as Map)
+	// 	}
 
-		const pointToLayer = (school: Feature, latlng: LatLngExpression) => {
-			const icon = Leaflet.icon({
-				iconSize: [27, 27],
-				iconAnchor: [13, 27],
-				popupAnchor: [1, -24],
-				iconUrl: existingSchoolSvg,
-			})
-			return Leaflet.marker(latlng, { icon })
-		}
+	// 	const pointToLayer = (school: Feature, latlng: LatLngExpression) => {
+	// 		const icon = Leaflet.icon({
+	// 			iconSize: [27, 27],
+	// 			iconAnchor: [13, 27],
+	// 			popupAnchor: [1, -24],
+	// 			iconUrl: existingSchoolSvg,
+	// 		})
+	// 		return Leaflet.marker(latlng, { icon })
+	// 	}
 
-		Leaflet.geoJSON(schoolsData, {
-			onEachFeature,
-			pointToLayer,
-		}).addTo(map.current as Map)
-	}, [schoolsData])
+	// 	Leaflet.geoJSON(schoolsData, {
+	// 		onEachFeature,
+	// 		pointToLayer,
+	// 	}).addTo(map.current as Map)
+	// }, [schoolsData])
+
+	// // Наносим на карту информацию о потребности в школах (тепловая карта).
+	// useEffect(() => {
+	// 	const points = DataGenerator.overPopulationHeatData()
+
+	// 	Leaflet.heatLayer(points, {
+	// 		minOpacity: 0.3,
+	// 	}).addTo(map.current as Map)
+	// }, [])
 
 	// Наносим на карту информацию о потребности в школах (тепловая карта).
 	useEffect(() => {
-		const points = DataGenerator.overPopulationHeatData()
-
-		Leaflet.heatLayer(points, {
-			minOpacity: 0.3,
+		Leaflet.geoJSON(cellsData.features, {
+			style: (cell) => {
+				return {
+					color: 'black',
+					// @ts-ignore
+					fillColor: getColor(cell.self_overload),
+					weight: 2,
+				}
+			},
 		}).addTo(map.current as Map)
 	}, [])
 
